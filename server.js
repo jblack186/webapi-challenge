ef000e9b072da6420549c02a4b4588ccf7e74781
Jamison Blackwell <blackwellj1040@gmail.com>
2019-08-16 20:09:41 -0400

    test

--- server.js
+++ server.js
@@ -11,3 +11,15 @@ server.get('/', (req, res) => {
 
 
 module.exports = server;
+const express = require('express');
+const helmet = require('helmet');
+
+
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+
+module.exports = server;
